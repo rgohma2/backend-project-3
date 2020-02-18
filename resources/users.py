1f7309b9a2b458011f6cfb0e2b07f7f0fb38ca02
Randy Gohmann <rgohma2@gmail.com>
2020-02-17 23:11:17 -0600

    can now register new user with encrypted password

--- resources/users.py
+++ resources/users.py
@@ -1,7 +1,9 @@
 import models
 
 from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash, check_password_hash
 from playhouse.shortcuts import model_to_dict
+from flask_login import login_user
 
 
 users = Blueprint('users', 'users')
@@ -15,4 +17,42 @@ def register():
 	payload = request.get_json()
 	print(payload)
 
-	return 'working'
+	payload['email'] = payload['email'].lower()
+
+	try:
+
+		models.User.get(models.User.email == payload['email'])
+
+		return jsonify(
+				data={},
+				message='there is already a user registered with that email',
+				status=401
+			), 401
+
+	except models.DoesNotExist:
+		payload['password'] = generate_password_hash(payload['password'])
+		new_user = models.User.create(**payload)
+		login_user(new_user)
+
+		user_dict = model_to_dict(new_user)
+		user_dict.pop('password')
+
+		return jsonify(
+				data=user_dict,
+				message='sucessfully registered new user with email: {}'.format(user_dict['email']),
+				status=201
+			), 201
+
+
+
+
+
+
+
+
+
+
+
+
+
+
