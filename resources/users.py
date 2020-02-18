6610dd62058209e530c5433fd0fc60251b2c6d45
Randy Gohmann <rgohma2@gmail.com>
2020-02-18 09:22:37 -0600

    login route works, weird indentation error to come back and fix with jsonify indentation error in else statement

--- resources/users.py
+++ resources/users.py
@@ -42,6 +42,44 @@ def register():
 				message='sucessfully registered new user with email: {}'.format(user_dict['email']),
 				status=201
 			), 201
+		return jsonify(
+				data=user_dict,
+				message='sucessfully registered new user with email: {}'.format(user_dict['email']),
+				status=201
+			), 201
+
+@users.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	payload['email'] = payload['email'].lower()
+
+	try:
+		user = models.User.get(models.User.email == payload['email'])
+		user_dict = model_to_dict(user)
+		password_match = check_password_hash(user_dict['password'], payload['password'])
+
+		if password_match:
+			login_user(user)
+			user_dict.pop('password')
+			return jsonify(
+					data={},
+					message='sucessfully logged in with email: {}'.format(user_dict['email']),
+					status=200
+				), 200
+		else:
+			return 'email or password does not match'
+	    	# return jsonify( data={}, message=('email or password does not match'), status=401), 401
+	    		
+
+	except models.DoesNotExist:
+		print('bad email')
+		return jsonify(
+				data={},
+	    		message='email or password does not match',
+	    		status=401
+			), 401
+
+
 
 
 
