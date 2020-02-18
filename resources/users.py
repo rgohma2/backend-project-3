a87d039bbd521e4b124a7094777d06139fad2597
Randy Gohmann <rgohma2@gmail.com>
2020-02-17 22:58:56 -0600

    users register route prints payload to terminal

--- resources/users.py
+++ resources/users.py
@@ -1,9 +1,18 @@
 import models
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
+
 
 users = Blueprint('users', 'users')
 
 @users.route('/', methods=['GET'])
 def test():
 	return 'users resource working!'
+
+@users.route('/register', methods=['POST'])
+def register():
+	payload = request.get_json()
+	print(payload)
+
+	return 'working'
